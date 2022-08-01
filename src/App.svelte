aa855acf7512d3b45d349b78eca6b27c419ed317
Natouche68 <ngrossheny@gmail.com>
2022-08-01 15:34:24 +0200

    Create a simple home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,31 @@
-Hello World !
+<script lang="ts">
+</script>
+
+<div class="app">
+	<div class="title">Theme Generator</div>
+	<div class="subtitle">Generates themes for game jams, or any other competition</div>
+</div>
+
+<style>
+	.app {
+		width: 100vw;
+		height: 100vh;
+		background: #00a2e7;
+		color: #fff;
+		font-family: 'Josefin Sans', 'Arial', sans-serif;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 4.8em;
+		font-weight: 700;
+	}
+
+	.subtitle {
+		font-size: 2em;
+		font-weight: 500;
+	}
+</style>
